11670af9d4bdbb0c1b9a8949a65dae9e9b5854a3
Santiago Cabrera <45210638+stiagocab@users.noreply.github.com>
2020-07-08 18:43:31 -0500

    refactor tailwind

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import "./css/tailwind.css"
+import "./css/tailwind.output.css"
 
 let client = new ApolloClient({
   uri: "https://api.spacex.land/graphql/",
