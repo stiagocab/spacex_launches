e285f131975d8a00960a06ea00c596b3e2318c99
Santiago Cabrera <stiagocabrera@gmail.com>
2020-07-08 16:53:49 -0500

    Updates

---
+++ precache-manifest.b4ec77e204956dd4649051e0c59c8bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e075384c56ced12717f01d9692516ef",
+    "url": "/stiagocab/spacex_launches/index.html"
+  },
+  {
+    "revision": "0eb397124b56dc61add6",
+    "url": "/stiagocab/spacex_launches/static/css/main.7ba86e95.chunk.css"
+  },
+  {
+    "revision": "2f7f87efc99015e71510",
+    "url": "/stiagocab/spacex_launches/static/js/2.73fe22af.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/stiagocab/spacex_launches/static/js/2.73fe22af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eb397124b56dc61add6",
+    "url": "/stiagocab/spacex_launches/static/js/main.594223b9.chunk.js"
+  },
+  {
+    "revision": "0aee553fb44e4e83f37e",
+    "url": "/stiagocab/spacex_launches/static/js/runtime-main.44aa3381.js"
+  }
+]);
